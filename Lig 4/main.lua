ce54ada2f04fca35cce5f49541ccd0f822c581a8
PDTCCLF <80176084+PDTCCLF@users.noreply.github.com>
2023-10-22 18:56:05 -0300

    Removendo arquivos antigos e fazendo atualizações

--- Lig 4/main.lua
+++ Lig 4/main.lua
@@ -31,63 +31,21 @@ local duracaoFogo = 1
 local maxPontos = 3
 
 
-function drawCenteredText(rectX, rectY, rectWidth, rectHeight, text)
-	local font       = love.graphics.getFont()
-	local textWidth  = font:getWidth(text)
-	local textHeight = font:getHeight()
-	love.graphics.print(text, rectX+rectWidth/2, rectY+rectHeight/2, 0, 1, 1, textWidth/2, textHeight/2)
-end
-
-
 function love.load()
   love.window.setMode(D,D*ny/nx,{resizable = true})
   love.window.setTitle("Lig 4")
-  love.graphics.setNewFont("PixelOperator8.ttf",tamFont)
   
   
   --***************************Carregamento dos áudios*********************************************
   
-  music=love.audio.newSource("musica/Bomberman 64 Music Opening Theme.mp3", "stream")
-  --music:play()
 
-  local pathSounds="efeitos sonoros/"
-  local soundsNomes = {"minecraft-tnt-explosion.mp3","Death sound.mp3"}
+  --local pathSounds="efeitos sonoros/"
+  --local soundsNomes = {"peca.mp3","Victory Sound Effect.mp3"}
   
-  soundEffects = {}
-  for i, sound in pairs(soundsNomes) do
-    table.insert(soundEffects,love.audio.newSource(pathSounds..sound, "static"))
-  end
-  
-  
-  --***************************Carregamento das imagens*********************************************
- 
-  --Background
-  local background_img=love.graphics.newImage("imagens/mapa/BackgroundTile.png")
-  background = {}
-  background["image"] = background_img
-  background["sx"] = 1/background_img:getWidth()
-  background["sy"] = 1/background_img:getHeight()
-  
-  --Bombas
-  local pathBombs="imagens/bomba/"
-  local imgNomesBombs = {"Bomb_f01.png","Bomb_f02.png","Bomb_f03.png"}
-  imgBombs = {}
-  for i, img in pairs(imgNomesBombs) do
-    table.insert(imgBombs,love.graphics.newImage(pathBombs..img))
-  end
-  iBomb=1
-  
-  --Paredes
-  local area1,area2,area3,area4=love.graphics.newImage("imagens/mapa/SolidBlock.png"),love.graphics.newImage("imagens/mapa/tijolo.png"),love.graphics.newImage("imagens/mapa/fogo.png"),love.graphics.newImage("imagens/mapa/tijoloFogo.png")
-  local imagens = {area1,area2,imgBombs[iBomb],area3,area4}
-  walls = {}
-  for i, imagem in ipairs(imagens) do
-    local wall = {}
-    wall["image"] = imagem
-    wall["sx"] = 1/imagem:getWidth()
-    wall["sy"] = 1/imagem:getHeight()
-    table.insert(walls,wall)
-  end
+  --soundEffects = {}
+  --for i, sound in pairs(soundsNomes) do
+    --table.insert(soundEffects,love.audio.newSource(pathSounds..sound, "static"))
+  --end
  
   --***************************Criação do mapa*********************************************
   
@@ -114,28 +72,13 @@ function love.update(dt)
       
       M:up()
     end  
-    
-    tempoBomb = tempoBomb + dt
-    
-    if tempoBomb > 1/FPSBomb then
-      tempoBomb = tempoBomb - 1/FPSBomb
-      iBomb = iBomb + 1
-      if iBomb > #imgBombs then
-        iBomb = 1
-      end
-      walls[3].image = imgBombs[iBomb]
-    end
-    
+  end  
 end
-  
-end
-
 
 function love.keypressed(key)
  if estado == "pause" then
     if key == "space" or key == "return" then
       estado = "jogo"
-      --music:play()
     end
   end
   if key == "." then
@@ -155,8 +98,8 @@ function love.draw()
   --Desenho do mapa
   
   if estado == "jogo" then
-    --if not music:isPlaying() then
-      --music:play()
+    --if then
+
     --end
   end
   
@@ -175,7 +118,6 @@ function love.resize(w, h)
     D = h*nx/ny
   end
   tamFont = D/15
-  love.graphics.setNewFont("PixelOperator8.ttf",tamFont)
   consts.D = D
 end
 
